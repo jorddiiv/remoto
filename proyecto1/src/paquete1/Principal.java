fae6092b7e8ff8f1de6cca6a64c6b7cb8b07e7b3
DAW <DAW@DESKTOP-U4FTC0L>
2023-05-05 14:06:48 +0200

    commit 2

--- proyecto1/src/paquete1/Principal.java
+++ proyecto1/src/paquete1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Ejemplo de control de versiones con git");
 		System.out.println("2do commit");
+		System.out.println("3ro commit");
 	}
 
 }
